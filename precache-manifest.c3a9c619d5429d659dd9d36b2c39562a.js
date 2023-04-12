ea6e8bcdd19da52174449157d46db04314a516cd
ethan.w.foshee <skyhailer2@gmail.com>
2023-04-11 20:32:03 -0500

    Updates

---
+++ precache-manifest.c3a9c619d5429d659dd9d36b2c39562a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5317c549004502cf0f29c89363752780",
+    "url": "/redux-work-prt-2/index.html"
+  },
+  {
+    "revision": "93201be6c5c01ec6d1b3",
+    "url": "/redux-work-prt-2/static/css/main.be9515fe.chunk.css"
+  },
+  {
+    "revision": "1fb404c133b4fff7ba8a",
+    "url": "/redux-work-prt-2/static/js/2.68a69ba7.chunk.js"
+  },
+  {
+    "revision": "93201be6c5c01ec6d1b3",
+    "url": "/redux-work-prt-2/static/js/main.d5966922.chunk.js"
+  },
+  {
+    "revision": "badc4ea3e3d185daafc7",
+    "url": "/redux-work-prt-2/static/js/runtime~main.1e5c2cd9.js"
+  }
+]);
